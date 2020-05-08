afb7272277b22e3d0858afe3a333108cf6e3e259
Raphael Tetreault <raphael.tetreault@outlook.com>
2020-05-07 23:35:04 -0400

    Fix scene saving persistence issue

--- Manifold_Unity/Assets/Root/GameCube/F-Zero GX/COLI/ColiStageGenerator.cs
+++ Manifold_Unity/Assets/Root/GameCube/F-Zero GX/COLI/ColiStageGenerator.cs
@@ -35,7 +35,8 @@ public class ColiStageGenerator : ImportSobjs<ColiSceneSobj>
             var scenePath = $"Assets/_Scene/{sceneName}.unity";
             var scene = EditorSceneManager.NewScene(NewSceneSetup.DefaultGameObjects, NewSceneMode.Single);
             EditorSceneManager.SaveScene(scene, scenePath);
-            EditorSceneManager.OpenScene(scenePath);
+            // Keep reference of new scene
+            scene = EditorSceneManager.OpenScene(scenePath);
 
             foreach (var gobj in coli.scene.gameObjects)
             {
@@ -87,7 +88,7 @@ public class ColiStageGenerator : ImportSobjs<ColiSceneSobj>
 
                 count++;
             }
-            AssetDatabase.SaveAssets();
+            EditorSceneManager.SaveScene(scene, scenePath, false);
             EditorUtility.ClearProgressBar();
             AssetDatabase.Refresh();
         }
