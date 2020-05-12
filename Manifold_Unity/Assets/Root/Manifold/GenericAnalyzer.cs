5e730d12c2b38ad179b3851b541ec4f07d7d1427
Raphael Tetreault <raphael.tetreault@outlook.com>
2020-05-12 17:31:33 -0400

    Removed unused classes

--- Manifold_Unity/Assets/Root/Manifold/GenericAnalyzer.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class GenericAnalyzer : MonoBehaviour
-{
-    // TODO:
-    // Reg tool to import any file out to hex
-}
