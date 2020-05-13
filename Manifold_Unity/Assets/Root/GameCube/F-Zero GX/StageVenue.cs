5d6c586d5edf537b7ac9c6ebc0f7760d2f894a64
Raphael Tetreault <raphael.tetreault@outlook.com>
2020-05-13 15:38:31 -0400

    Add StageVenue enum with description

---
+++ Manifold_Unity/Assets/Root/GameCube/F-Zero GX/StageVenue.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel;
+
+public enum StageVenue
+{
+    [Description("Aeropolis")]
+    Aeropolis,
+    [Description("Big Blue")]
+    BigBlue,
+    [Description("Casino Palace")]
+    CasinoPalace,
+    [Description("Cosmo Terminal")]
+    CosmoTerminal,
+    [Description("Fire Field")]
+    FireField,
+    [Description("Green Plant")]
+    GreenPlant,
+    [Description("Lightning")]
+    Lightning,
+    [Description("Null")]
+    None,
+    [Description("Mute City")]
+    MuteCity,
+    [Description("Outer Space")]
+    OuterSpace,
+    [Description("Phantom Road")]
+    PhantomRoad,
+    [Description("Port Town")]
+    PortTown,
+    [Description("Sand Ocean")]
+    SandOcean,
+    [Description("Mute City")]
+    Win,
+    [Description("Mute City")]
+    Win_GX,
+}
