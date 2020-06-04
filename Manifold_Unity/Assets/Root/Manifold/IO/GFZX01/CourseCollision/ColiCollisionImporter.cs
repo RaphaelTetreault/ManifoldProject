5f900d03772276548fb19ce81f93d5a88cce41ae
Raphael Tetreault <raphael.tetreault@outlook.com>
2020-06-04 00:11:17 -0400

    Add working WIP object collision importer

--- Manifold_Unity/Assets/Root/Manifold/IO/GFZX01/CourseCollision/ColiCollisionImporter.cs
+++ Manifold_Unity/Assets/Root/Manifold/IO/GFZX01/CourseCollision/ColiCollisionImporter.cs
@@ -42,8 +42,7 @@ namespace Manifold.IO.GFZX01.CourseCollision
                     if (gobj.collisionBinding.collisionRelPtr != 0)
                     {
                         var mesh = CreateMesh(gobj);
-                        mesh.name = gobj.name;
-                        AssetDatabase.CreateAsset(mesh, $"Assets/{mesh.name}.asset");
+                        AssetDatabase.CreateAsset(mesh, $"Assets/{coli.name}_{mesh.name}.asset");
                         var prefab = CreatePrefabFromModel(mesh, "Assets/");
                         DestroyImmediate(prefab);
                     }
@@ -55,11 +54,10 @@ namespace Manifold.IO.GFZX01.CourseCollision
 
         public Mesh CreateMesh(GameCube.GFZX01.CourseCollision.GameObject gameObject)
         {
-            var name = gameObject.name;
             var collision = gameObject.collisionBinding.collision;
 
             var mesh = new Mesh();
-            //mesh.subMeshCount = 2;
+            mesh.name = gameObject.name;
             var submeshes = new SubMeshDescriptor[2];
 
             // TRIS
@@ -69,7 +67,7 @@ namespace Manifold.IO.GFZX01.CourseCollision
                 //
                 var vertices = new Vector3[vertCount];
                 //var normals = new Vector3[triCount];
-                var triangles = new int[vertCount];
+                var indices = new int[vertCount];
                 //
                 for (int i = 0; i < triCount; i++)
                 {
@@ -81,23 +79,23 @@ namespace Manifold.IO.GFZX01.CourseCollision
                     vertices[stride + 1] = collision.tris[i].vertex1;
                     vertices[stride + 2] = collision.tris[i].vertex2;
                     //
-                    triangles[stride + 0] = stride + 0;
-                    triangles[stride + 1] = stride + 1;
-                    triangles[stride + 2] = stride + 2;
+                    indices[stride + 0] = stride + 0;
+                    indices[stride + 1] = stride + 1;
+                    indices[stride + 2] = stride + 2;
                 }
                 var triSubmesh = new SubMeshDescriptor();
                 // Build submesh
                 triSubmesh.baseVertex = mesh.vertexCount; // 0
                 triSubmesh.firstVertex = mesh.vertexCount; // 0
-                triSubmesh.indexCount = triCount;
+                triSubmesh.indexCount = indices.Length;
                 triSubmesh.indexStart = mesh.triangles.Length; // 0
                 triSubmesh.topology = MeshTopology.Triangles;
-                triSubmesh.vertexCount = vertCount;
+                triSubmesh.vertexCount = vertices.Length;
 
                 // Append to mesh
                 var verticesConcat = mesh.vertices.Concat(vertices).ToArray();
                 //var normalsConcat = mesh.normals.Concat(normals).ToArray();
-                var trianglesConcat = mesh.triangles.Concat(triangles).ToArray();
+                var trianglesConcat = mesh.triangles.Concat(indices).ToArray();
 
                 // Assign values to mesh
                 mesh.vertices = verticesConcat;
@@ -110,49 +108,44 @@ namespace Manifold.IO.GFZX01.CourseCollision
             }
             // QUADS
             {
-                const int vertsPerQuad = 6;
                 var quadCount = collision.quadCount;
-                var vertCount = quadCount * vertsPerQuad;
+                var vertCount = quadCount * 4;
                 //
                 var vertices = new Vector3[vertCount];
                 //var normals = new Vector3[quadCount];
-                var quadTris = new int[vertCount];
+                var indices = new int[quadCount * 6];
                 //
                 for (int i = 0; i < quadCount; i++)
                 {
                     //normals[i] = collision.quads[i].normal;
 
-                    var stride = i * vertsPerQuad;
-                    //
-                    vertices[stride + 0] = collision.quads[i].vertex0;
-                    vertices[stride + 1] = collision.quads[i].vertex1;
-                    vertices[stride + 2] = collision.quads[i].vertex2;
-                    ////////////////////
-                    vertices[stride + 3] = collision.quads[i].vertex1;
-                    vertices[stride + 4] = collision.quads[i].vertex3;
-                    vertices[stride + 5] = collision.quads[i].vertex2;
-                    //
-                    quadTris[stride + 0] = stride + 0;
-                    quadTris[stride + 1] = stride + 1;
-                    quadTris[stride + 2] = stride + 2;
-                    /////////////////
-                    quadTris[stride + 1] = stride + 3;
-                    quadTris[stride + 2] = stride + 4;
-                    quadTris[stride + 3] = stride + 5;
+                    var vertStride = i * 4;
+                    vertices[vertStride + 0] = collision.quads[i].vertex0;
+                    vertices[vertStride + 1] = collision.quads[i].vertex1;
+                    vertices[vertStride + 2] = collision.quads[i].vertex2;
+                    vertices[vertStride + 3] = collision.quads[i].vertex3;
+
+                    var quadStride = i * 6;
+                    indices[quadStride + 0] = vertStride + 0;
+                    indices[quadStride + 1] = vertStride + 1;
+                    indices[quadStride + 2] = vertStride + 2;
+                    indices[quadStride + 3] = vertStride + 0;
+                    indices[quadStride + 4] = vertStride + 2;
+                    indices[quadStride + 5] = vertStride + 3;
                 }
                 var quadSubmesh = new SubMeshDescriptor();
                 // Build submesh
                 quadSubmesh.baseVertex = mesh.vertexCount;
                 quadSubmesh.firstVertex = mesh.vertexCount;
-                quadSubmesh.indexCount = quadCount;
+                quadSubmesh.indexCount = indices.Length;
                 quadSubmesh.indexStart = mesh.triangles.Length;
-                quadSubmesh.topology = MeshTopology.Quads;
-                quadSubmesh.vertexCount = vertCount;
+                quadSubmesh.topology = MeshTopology.Triangles;
+                quadSubmesh.vertexCount = vertices.Length;
 
                 // Append to mesh
                 var verticesConcat = mesh.vertices.Concat(vertices).ToArray();
                 //var normalsConcat = mesh.normals.Concat(normals).ToArray();
-                var trianglesConcat = mesh.triangles.Concat(quadTris).ToArray();
+                var trianglesConcat = mesh.triangles.Concat(indices).ToArray();
 
                 // Assign values to mesh
                 mesh.vertices = verticesConcat;
