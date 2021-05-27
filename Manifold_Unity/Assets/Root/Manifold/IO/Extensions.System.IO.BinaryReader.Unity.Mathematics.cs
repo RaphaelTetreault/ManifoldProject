0d865b7fb2c04aa0408058b570744a5d12f69525
Raphael Tetreault <raphael.tetreault@outlook.com>
2021-05-27 14:21:02 -0400

    Add Unity.Mathematics (2021/03/17 commit)

---
+++ Manifold_Unity/Assets/Root/Manifold/IO/Extensions.System.IO.BinaryReader.Unity.Mathematics.cs
@@ -0,0 +1,67 @@
+using System.IO;
+using Unity.Mathematics;
+
+namespace Manifold.IO
+{
+    public static partial class BinaryReaderExtensions
+    {
+        public static float2 ReadFloat2(this BinaryReader reader)
+        {
+            return new float2(
+                BinaryIoUtility.ReadFloat(reader),
+                BinaryIoUtility.ReadFloat(reader));
+        }
+
+        public static float3 ReadFloat3(this BinaryReader reader)
+        {
+            return new float3(
+                BinaryIoUtility.ReadFloat(reader),
+                BinaryIoUtility.ReadFloat(reader),
+                BinaryIoUtility.ReadFloat(reader));
+        }
+
+        public static float4 ReadFloat4(this BinaryReader reader)
+        {
+            return new float4(
+                BinaryIoUtility.ReadFloat(reader),
+                BinaryIoUtility.ReadFloat(reader),
+                BinaryIoUtility.ReadFloat(reader),
+                BinaryIoUtility.ReadFloat(reader));
+        }
+
+        public static quaternion ReadMathQuaternion(this BinaryReader reader)
+        {
+            return new quaternion(
+                BinaryIoUtility.ReadFloat(reader),
+                BinaryIoUtility.ReadFloat(reader),
+                BinaryIoUtility.ReadFloat(reader),
+                BinaryIoUtility.ReadFloat(reader));
+        }
+
+
+        // Function forwarding
+        public static float2 ReadX(this BinaryReader reader, ref float2 value)
+            => value = reader.ReadFloat2();
+
+        public static float3 ReadX(this BinaryReader reader, ref float3 value)
+            => value = reader.ReadFloat3();
+
+        public static float4 ReadX(this BinaryReader reader, ref float4 value)
+            => value = reader.ReadFloat4();
+
+        public static quaternion ReadX(this BinaryReader reader, ref quaternion value)
+            => value = reader.ReadMathQuaternion();
+
+        public static float2[] ReadX(this BinaryReader reader, ref float2[] value, int length)
+            => value = BinaryIoUtility.ReadNewArray(reader, length, ReadFloat2);
+
+        public static float3[] ReadX(this BinaryReader reader, ref float3[] value, int length)
+            => value = BinaryIoUtility.ReadNewArray(reader, length, ReadFloat3);
+
+        public static float4[] ReadX(this BinaryReader reader, ref float4[] value, int length)
+            => value = BinaryIoUtility.ReadNewArray(reader, length, ReadFloat4);
+
+        public static quaternion[] ReadX(this BinaryReader reader, ref quaternion[] value, int length)
+            => value = BinaryIoUtility.ReadNewArray(reader, length, ReadMathQuaternion);
+    }
+}
