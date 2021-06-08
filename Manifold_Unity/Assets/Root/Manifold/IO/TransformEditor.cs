7d7afd36dd84cdf5a2930776cefe9fec0688f650
Raphael Tetreault <raphael.tetreault@outlook.com>
2021-06-08 19:24:19 -0400

    Improve TransformEditor stability and look

--- Manifold_Unity/Assets/Root/Manifold/IO/TransformEditor.cs
+++ Manifold_Unity/Assets/Root/Manifold/IO/TransformEditor.cs
@@ -3,28 +3,50 @@ using UnityEditor;
 
 namespace Manifold.IO
 {
+    // TODO: cleanup this crap from 3 iterations...
     [CustomEditor(typeof(Transform))]
     public class TransformEditor : Editor
     {
+        SerializedProperty m_Position;
+        SerializedProperty m_Rotation;
+        SerializedProperty m_Scale;
+
+        const string positionLabel = "Position";
+        const string rotationLabel = "Rotation";
+        const string scaleLabel = "Scale";
+
+        public void OnEnable()
+        {
+            m_Position = serializedObject.FindProperty("m_LocalPosition");
+            m_Rotation = serializedObject.FindProperty("m_LocalRotation");
+            m_Scale = serializedObject.FindProperty("m_LocalScale");
+        }
         public override void OnInspectorGUI()
         {
-            var transform = (Transform)serializedObject.targetObject;
+            var transform = serializedObject.targetObject as Transform;
             serializedObject.Update();
 
             // DRAW LOCAL
-            transform.localPosition =
-                EditorGUILayout.Vector3Field("Local Position", transform.localPosition);
-            transform.localRotation =
-                Quaternion.Euler(EditorGUILayout.Vector3Field("Local Rotation", transform.localRotation.eulerAngles));
-            transform.localScale =
-                EditorGUILayout.Vector3Field("Local Scale", transform.localScale);
-            
+            EditorGUILayout.LabelField("Local", EditorStyles.boldLabel);
+            var pos = EditorGUILayout.Vector3Field(positionLabel, m_Position.vector3Value);
+            var rot = EditorGUILayout.Vector3Field(rotationLabel, m_Rotation.quaternionValue.eulerAngles);
+            var sca = EditorGUILayout.Vector3Field(scaleLabel, m_Scale.vector3Value);
+
             // DRAW GLOBAL
+            EditorGUILayout.LabelField("Global", EditorStyles.boldLabel);
             GUI.enabled = false;
-            EditorGUILayout.Vector3Field("Global Position", transform.position);
-            EditorGUILayout.Vector3Field("Global Rotation", transform.rotation.eulerAngles);
-            EditorGUILayout.Vector3Field("Global Scale", transform.lossyScale);
+            EditorGUILayout.Vector3Field(positionLabel, transform.position);
+            EditorGUILayout.Vector3Field(rotationLabel, transform.rotation.eulerAngles);
+            EditorGUILayout.Vector3Field(scaleLabel, transform.lossyScale);
             GUI.enabled = true;
+
+            if (GUI.changed)
+            {
+                Undo.RecordObject(transform, "Transform Changed");
+                transform.localPosition = pos;
+                transform.localEulerAngles = rot;
+                transform.localScale = sca;
+            }
 
             serializedObject.ApplyModifiedProperties();
         }
