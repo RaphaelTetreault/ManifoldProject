21357feff933f2fc4f64b7c5e8509f66f86ca23e
Raphael Tetreault <raphael.tetreault@outlook.com>
2021-06-11 22:54:17 -0400

    Add note from SMB2 community

--- Manifold_Unity/Assets/Root/GameCube/GFZ/CourseCollision/FogInterpolation.cs
+++ Manifold_Unity/Assets/Root/GameCube/GFZ/CourseCollision/FogInterpolation.cs
@@ -5,6 +5,10 @@ namespace GameCube.GFZ.CourseCollision
     /// </summary>
     public enum FogInterpolation
     {
+        // NOTES FROM SMB2 COMMUNITY:
+        // Fog type (Usually 0x04, although this can be any from _GXFogType - NONE: 0x00,
+        // LIN: 0x02, EXP: 0x04, EXP2: 0x05, REVEXP: 0x06, REVEXP2: 0x07)
+
         // Could be exponential?
         Cubic = 0x02000000,
         
