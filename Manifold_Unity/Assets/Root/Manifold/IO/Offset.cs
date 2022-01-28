b2fec69fefb3824fd16bff0690c94771492a0943
Raphael Tetreault <raphaeltetreault@hotmail.com>
2022-01-28 11:15:29 -0500

    Implement IOffset, Offset

--- Manifold_Unity/Assets/Root/Manifold/IO/Offset.cs
+++ Manifold_Unity/Assets/Root/Manifold/IO/Offset.cs
@@ -4,23 +4,85 @@ using System.IO;
 namespace Manifold.IO
 {
     [Serializable]
-    public struct Offset : IBinarySerializable
+    public struct Offset :
+        IBinarySerializable,
+        IEquatable<Offset>,
+        IOffset
     {
-        public int offset;
+        // FIELDS
+        public int addressOffset;
 
-        public void SeekAddress(BinaryReader reader, SeekOrigin seekOrigin = SeekOrigin.Current)
+        // CONSTRUCTORS
+        public Offset(int offset)
         {
-            reader.BaseStream.Seek(offset, seekOrigin);
+            this.addressOffset = offset;
+        }
+
+        public Offset(long offset)
+        {
+            this.addressOffset = (int)offset;
+        }
+
+
+        // PROPERTIES
+        public int AddressOffset => addressOffset;
+        public bool IsNotNull => addressOffset != 0;
+        public bool IsNull => addressOffset == 0;
+        public string Hexadecimal => $"{addressOffset:x8}";
+
+
+        // OPERATORS
+        public static implicit operator int(Offset offset)
+        {
+            return offset.addressOffset;
+        }
+
+        public static implicit operator Offset(int addressOffset)
+        {
+            return new Offset(addressOffset);
+        }
+
+        public static implicit operator Offset(long addressOffset)
+        {
+            return new Offset(addressOffset);
+        }
+
+        // METHODS
+
+        public Pointer CreatePointer(Pointer basePointer)
+        {
+            return new Pointer(basePointer + addressOffset);
         }
 
         public void Deserialize(BinaryReader reader)
         {
-            reader.ReadX(ref offset);
+            reader.ReadX(ref addressOffset);
         }
 
         public void Serialize(BinaryWriter writer)
         {
-            writer.WriteX(offset);
+            writer.WriteX(addressOffset);
         }
+
+        public override string ToString()
+        {
+            return Hexadecimal;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Equals((Offset)obj);
+        }
+
+        public bool Equals(Offset obj)
+        {
+            return obj.addressOffset == this.addressOffset;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
     }
 }
