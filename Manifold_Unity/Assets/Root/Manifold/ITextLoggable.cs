a3378815a47f9b662cacc01fc1c8b8216cefaa0f
Raphael Tetreault <raphaeltetreault@hotmail.com>
2022-01-27 21:07:01 -0500

    Add ITextLoggable

---
+++ Manifold_Unity/Assets/Root/Manifold/ITextLoggable.cs
@@ -0,0 +1,17 @@
+﻿namespace Manifold
+{
+    public interface ITextLoggable
+    {
+        /// <summary>
+        /// Prints this value without newlines.
+        /// </summary>
+        /// <returns></returns>
+        public string ToStringSingleLine();
+
+        /// <summary>
+        /// Prints this value, possibly including newlines.
+        /// </summary>
+        /// <returns></returns>
+        public string ToStringMultiLine();
+    }
+}
