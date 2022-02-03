cd3741abda133e1ca96b400e11dcac093327c8f6
Raphael Tetreault <raphaeltetreault@hotmail.com>
2022-02-03 15:00:24 -0500

    m: make Const.Resources use properties to count references in Visual Studio

--- Manifold_Unity/Assets/Root/Manifold/EditorTools/Const.Resources.cs
+++ Manifold_Unity/Assets/Root/Manifold/EditorTools/Const.Resources.cs
@@ -8,12 +8,20 @@ namespace Manifold.EditorTools
     {
         public static partial class Resources
         {
-            public const string Cube        = "Cube.fbx";
-            public const string Capsule     = "Capsule.fbx";
-            public const string Cylinder    = "Cylinder.fbx";
-            public const string Place       = "Place.fbx";
-            public const string Sphere      = "Sphere.fbx";
-            public const string Quad        = "Quad.fbx";
+            private const string cube        = "Cube.fbx";
+            private const string capsule     = "Capsule.fbx";
+            private const string cylinder    = "Cylinder.fbx";
+            private const string plane       = "Plane.fbx";
+            private const string sphere      = "Sphere.fbx";
+            private const string quad        = "Quad.fbx";
+
+            // Use Properties to identify references
+            public static string Cube => cube;
+            public static string Capsule => capsule;
+            public static string Cylinder => cylinder;
+            public static string Plane => plane;
+            public static string Sphere => sphere;
+            public static string Quad => quad;
         }
     }
 }
