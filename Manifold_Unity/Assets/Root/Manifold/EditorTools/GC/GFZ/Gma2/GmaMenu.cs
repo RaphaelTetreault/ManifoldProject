6fa54c42117c8cb5873aab60fe1f93b4e1be6acb
RaphaelTetreault <raphael.tetreault@outlook.com>
2022-02-24 23:38:16 -0500

    Add class to debug new Gma2 from Unity menu

---
+++ Manifold_Unity/Assets/Root/Manifold/EditorTools/GC/GFZ/Gma2/GmaMenu.cs
@@ -0,0 +1,44 @@
+﻿using GameCube.GFZ.Gma2;
+using Manifold;
+using Manifold.IO;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEditor;
+using UnityEngine;
+
+namespace Manifold.EditorTools.GC.GFZ.Gma2
+{
+    public static class GmaMenu
+    {
+        [MenuItem(Const.Menu.Manifold + "GMA/Import Single TEST")]
+        public static void TextImportGmaFixed()
+        {
+            var settings = GfzProjectWindow.GetSettings();
+            var folder = settings.StageDir;
+            var path = Path.Combine(folder, "st01.gma");
+            TestImportGma(path);
+        }
+
+        public static void TestImportGma(string filepath)
+        {
+            using (var reader = new BinaryReader(File.OpenRead(filepath)))
+            {
+                var gma = new Gma();
+                gma.Deserialize(reader);
+
+                // 
+                int index = 0;
+                foreach (var model in gma.Models)
+                {
+                    Debug.Log($"[{++index}] {model.Name}");
+                }
+            }
+        }
+
+
+    }
+}
