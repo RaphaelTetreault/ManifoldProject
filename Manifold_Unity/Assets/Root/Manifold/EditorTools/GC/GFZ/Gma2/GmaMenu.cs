39b6f7074f80c954f837cb8bb83faa6a7d24c1ca
RaphaelTetreault <raphael.tetreault@outlook.com>
2022-03-01 23:41:22 -0500

    Add generic helper for testing file IO operations, iterating over files

--- Manifold_Unity/Assets/Root/Manifold/EditorTools/GC/GFZ/Gma2/GmaMenu.cs
+++ Manifold_Unity/Assets/Root/Manifold/EditorTools/GC/GFZ/Gma2/GmaMenu.cs
@@ -11,67 +11,34 @@ namespace Manifold.EditorTools.GC.GFZ.Gma2
 {
     public static class GmaMenu
     {
-        public static void AllFileLoadLoop(string title, Action<string> loadAction)
+        public static void GmaFileActionLoop(string title, TestFileIO.FileAction fileAction)
         {
             var settings = GfzProjectWindow.GetSettings();
             var folder = settings.RootFolder;
             var filePaths = Directory.GetFiles(folder, "*.gma", SearchOption.AllDirectories);
-            int index = 0;
-            float count = filePaths.Length;
-            int countFormat = count.ToString().Length;
-            foreach (var filePath in filePaths)
-            {
-                index++;
-                string info = $"[{index.ToString().PadLeft(countFormat)}/{count}] {filePath}";
-                float progress = index / count;
-                bool cancel = EditorUtility.DisplayCancelableProgressBar(title, info, progress);
-                if (cancel)
-                    break;
-
-                loadAction(filePath);
-            }
-            EditorUtility.ClearProgressBar();
+            TestFileIO.FileActionLoop(title, filePaths, fileAction);
         }
 
-
-        //[MenuItem(Const.Menu.Manifold + "GMA/Import Single TEST")]
-        //public static void TestImportGmaFixed()
-        //{
-        //    var settings = GfzProjectWindow.GetSettings();
-        //    var folder = settings.StageDir;
-        //    var path = Path.Combine(folder, "st01.gma");
-        //    LoadGma(path);
-        //}
 
         [MenuItem(Const.Menu.Manifold + "GMA/LoadSave to Disk")]
         public static void LoadSaveToDisk()
         {
-            AllFileLoadLoop("Load/Save byte-for-byte", LoadSaveToDisk);
+            GmaFileActionLoop("Load/Save byte-for-byte", LoadSaveToDisk);
         }
-        // TODO: make this generic! Other files can use this
         public static void LoadSaveToDisk(string filePath)
         {
             // Get output parameters
             var settings = GfzProjectWindow.GetSettings();
-            var folder = settings.FileOutput;
-            // Get file names
-            var name = Path.GetFileNameWithoutExtension(filePath);
-            var ext = Path.GetExtension(filePath);
-            var inputFilePath = Path.Combine(folder, $"{name}{ext}");
-            var outputFilePath = Path.Combine(folder, $"{name}-export{ext}");
-            // Load GMA, save a copy
-            var gma = GmaIO.LoadGMA(filePath);
-            File.Copy(filePath, inputFilePath, true);
-            // Save own GMA
-            GmaIO.SaveGMA(gma, outputFilePath);
-            Debug.Log($"Read '{filePath}', Wrote '{outputFilePath}'");
+            var rootPath = settings.RootFolder;
+            var outputPath = settings.FileOutput + "/gma";
+            TestFileIO.LoadSaveToDisk(filePath, rootPath, outputPath, GmaIO.LoadGMA, GmaIO.SaveGMA);
         }
 
 
         [MenuItem(Const.Menu.Manifold + "GMA/Test Roundtrip Byte For Byte")]
         public static void RoundtripByteForByte()
         {
-            AllFileLoadLoop("Load/Save byte-for-byte", RoundtripByteForByte);
+            GmaFileActionLoop("Load/Save byte-for-byte", RoundtripByteForByte);
         }
         public static void RoundtripByteForByte(string filePath)
         {
@@ -83,7 +50,7 @@ namespace Manifold.EditorTools.GC.GFZ.Gma2
         [MenuItem(Const.Menu.Manifold + "GMA/Import All TEST")]
         public static void LoadSaveToMemory()
         {
-            AllFileLoadLoop("Load/Save All GMA", LoadSaveGma);
+            GmaFileActionLoop("Load/Save All GMA", LoadSaveGma);
         }
         public static void LoadSaveGma(string filePath)
         {
---
+++ Manifold_Unity/Assets/Root/Manifold/EditorTools/TestFileIO.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace Manifold.EditorTools
+{
+    public static class TestFileIO
+    {
+        public delegate void FileAction(string filePath);
+        public delegate TValue LoadFile<TValue>(string filePath);
+        public delegate void SaveFile<TValue>(TValue value, string filePath);
+
+        /// <summary>
+        /// Calls the <paramref name="fileAction"/> on every file in <paramref name="filePaths"/>.
+        /// </summary>
+        /// <param name="title">The title that appears on the progress bar.</param>
+        /// <param name="filePaths">The files paths to use in the supplied action.</param>
+        /// <param name="fileAction">The action to perform using each file path.</param>
+        public static void FileActionLoop(string title, string[] filePaths, FileAction fileAction)
+        {
+            int index = 0;
+            float count = filePaths.Length;
+            int countFormat = count.ToString().Length;
+            foreach (var filePath in filePaths)
+            {
+                index++;
+                string info = $"[{index.ToString().PadLeft(countFormat)}/{count}] {filePath}";
+                float progress = index / count;
+                bool cancel = EditorUtility.DisplayCancelableProgressBar(title, info, progress);
+                if (cancel)
+                    break;
+
+                fileAction(filePath);
+            }
+            EditorUtility.ClearProgressBar();
+        }
+
+        /// <summary>
+        /// Loads the file at <paramref name="filePath"/> and saves it to <paramref name="outputPath"/>. A
+        /// carbon copy of the loaded file is saved in the same output directory.
+        /// </summary>
+        /// <typeparam name="T">The type used by the load and save functions.</typeparam>
+        /// <param name="filePath">The file path to load from.</param>
+        /// <param name="rootPath">The root path of the loaded file. Used to make absolute path a relative path.</param>
+        /// <param name="outputPath">The output directory for files.</param>
+        /// <param name="load">The function which loads and returns the structure.</param>
+        /// <param name="save">The action that saves the structure to disk.</param>
+        public static void LoadSaveToDisk<T>(string filePath, string rootPath, string outputPath, LoadFile<T> load, SaveFile<T> save)
+        {
+            // Get directory to file relative to rootFolder
+            var folderPath = Path.GetDirectoryName(filePath);
+            folderPath = folderPath.Remove(0, rootPath.Length);
+            // Get file parameters
+            var name = Path.GetFileNameWithoutExtension(filePath);
+            var ext = Path.GetExtension(filePath);
+            // Create path to loaded file, saved file
+            var inputFilePath = Path.Combine(outputPath, folderPath, $"{name}{ext}");
+            var outputFilePath = Path.Combine(outputPath, folderPath, $"{name}.export{ext}");
+            // Make sure the directory exists for the output files
+            var outputDirectory = Path.GetDirectoryName(inputFilePath);
+            if (!Directory.Exists(outputDirectory))
+                Directory.CreateDirectory(outputDirectory);
+
+            //
+            var value = load(filePath);
+            save(value, outputFilePath);
+
+            // Make copy of file
+            File.Copy(filePath, inputFilePath, true);
+
+            Debug.Log($"Read '{filePath}', Wrote '{outputFilePath}'");
+        }
+
+    }
+}
