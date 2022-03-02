0364f9695894df0c903c231dfc461dbcf45b90c1
RaphaelTetreault <raphael.tetreault@outlook.com>
2022-03-02 00:12:53 -0500

    Add generic methods for LoadFile which do not require new()

--- Manifold_Unity/Assets/Root/Manifold/IO/BinarySerializableIO.cs
+++ Manifold_Unity/Assets/Root/Manifold/IO/BinarySerializableIO.cs
@@ -10,6 +10,7 @@ namespace Manifold.IO
         /// <summary>
         /// 
         /// </summary>
+        /// <typeparam name="TBinarySerializable"></typeparam>
         /// <param name="filePath"></param>
         /// <returns></returns>
         public static TBinarySerializable LoadFile<TBinarySerializable>(string filePath)
@@ -25,8 +26,57 @@ namespace Manifold.IO
         }
 
         /// <summary>
+        /// Loads file at and stores valie in <paramref name="binarySerializable"/> without creating a new instance.
+        /// </summary>
+        /// <typeparam name="TBinarySerializable">A type implementing the IBinarySerialize interface.</typeparam>
+        /// <param name="filePath">The file path to load from.</param>
+        /// <param name="binarySerializable">The instance to deserialize the file to.</param>
+        /// <returns></returns>
+        public static TBinarySerializable LoadFile<TBinarySerializable>(string filePath, TBinarySerializable binarySerializable)
+            where TBinarySerializable : IBinarySerializable, IFile
+        {
+            using (var reader = new BinaryReader(File.OpenRead(filePath)))
+            {
+                binarySerializable.FileName = Path.GetFileName(filePath);
+                binarySerializable.Deserialize(reader);
+                return binarySerializable;
+            }
+        }
+
+        /// <summary>
         /// 
         /// </summary>
+        /// <typeparam name="TBinarySerializable"></typeparam>
+        /// <param name="filePaths"></param>
+        /// <param name="binarySerializables"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException"></exception>
+        public static IEnumerable<TBinarySerializable> LoadFile<TBinarySerializable>(string[] filePaths, TBinarySerializable[] binarySerializables)
+            where TBinarySerializable : IBinarySerializable, IFile, new()
+        {
+            // Make sure arays are same length
+            if (filePaths.Length != binarySerializables.Length)
+                throw new ArgumentException($"{nameof(filePaths)} and {nameof(binarySerializables)} must have the same length.");
+
+            // Make sure binarySerializables passed in are not null
+            foreach (var binarySerializable in binarySerializables)
+                if (binarySerializable is null)
+                    throw new ArgumentException($"All instances in {nameof(binarySerializables)} must not be null.");
+
+            // Load
+            for (int i = 0; i < filePaths.Length; i++)
+            {
+                var value = LoadFile(filePaths[i], binarySerializables[i]);
+                yield return value;
+            }
+
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="TBinarySerializable"></typeparam>
         /// <param name="filePaths"></param>
         /// <returns></returns>
         public static IEnumerable<TBinarySerializable> LoadFile<TBinarySerializable>(params string[] filePaths)
@@ -42,8 +92,9 @@ namespace Manifold.IO
         /// <summary>
         /// 
         /// </summary>
-        /// <param name="filePath"></param>
+        /// <typeparam name="TBinarySerializable"></typeparam>
         /// <param name="binarySerializable"></param>
+        /// <param name="filePath"></param>
         public static void SaveFile<TBinarySerializable>(TBinarySerializable binarySerializable, string filePath)
             where TBinarySerializable : IBinarySerializable, IFile
         {
@@ -57,6 +108,7 @@ namespace Manifold.IO
         /// <summary>
         /// 
         /// </summary>
+        /// <typeparam name="TBinarySerializable"></typeparam>
         /// <param name="rootPath"></param>
         /// <param name="binarySerializables"></param>
         /// <returns></returns>
