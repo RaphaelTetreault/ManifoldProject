96fd531b693e04d8b4859e8faf5f3cfb207aecde
RaphaelTetreault <raphael.tetreault@outlook.com>
2022-03-04 23:27:10 -0500

    Delete old GMA editor tools scripts

--- Manifold_Unity/Assets/Root/Manifold/EditorTools/GC/GFZ/GMA/GmaSobj.cs
+++
@@ -1,9 +0,0 @@
-﻿//using GameCube.GFZ.GMA;
-
-//namespace Manifold.EditorTools.GC.GFZ.GMA
-//{
-//    [UnityEngine.CreateAssetMenu(menuName = Const.Menu.GfzGMA + "GMA")]
-//    // TODO: refactor and properly commit this as GmaSobj
-//    // git doesn't know how to push this as it is case insensitive.
-//    public class GmaSobj : FileAssetWrapper<Gma> { }
-//}
