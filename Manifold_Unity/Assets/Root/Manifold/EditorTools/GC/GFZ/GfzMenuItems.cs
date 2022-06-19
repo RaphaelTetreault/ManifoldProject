1ae1161eab8e660ff2d9635a6248176c448bac6a
Raphaël Tétreault <raphaeltetreault@outlook.com>
2022-06-19 00:12:04 -0400

    m: Genericize menu parameter in menu item string constants

--- Manifold_Unity/Assets/Root/Manifold/EditorTools/GC/GFZ/GfzMenuItems.cs
+++ Manifold_Unity/Assets/Root/Manifold/EditorTools/GC/GFZ/GfzMenuItems.cs
@@ -4,41 +4,41 @@ namespace Manifold.EditorTools.GC.GFZ
     {
         public static class Colliders
         {
-            public const string ColiMenu = Const.Menu.Manifold + "Colliders/";
+            public const string Menu = Const.Menu.Manifold + "Colliders/";
             // ColliderImport
-            public const string Import = ColiMenu + "Create collider meshes";
-            public const string Import256 = ColiMenu + "Create 256 static collider meshes (single scene, select type)";
+            public const string Import = Menu + "Create collider meshes";
+            public const string Import256 = Menu + "Create 256 static collider meshes (single scene, select type)";
             public const int ImportPriority = 1;
             public const int Import256Priority = 101;
         }
 
         public static class GMA
         {
-            public const string GmaMenu = Const.Menu.Manifold + "GMA/";
+            public const string Menu = Const.Menu.Manifold + "GMA/";
             // GmaImport
-            public const string ImportGma = GmaMenu + "Import all models from Source Folder";
-            public const string ImportGmaAllRegions = GmaMenu + "Test - Import all models from all regions";
+            public const string ImportGma = Menu + "Import all models from Source Folder";
+            public const string ImportGmaAllRegions = Menu + "Test - Import all models from all regions";
             public const int ImportGmaPriority = 1;
             public const int ImportGmaAllRegionsPriority = 101;
             // GmaTestMenuItems
-            public const string LoadSaveToDisk = GmaMenu + "Test - Load\\Save source folder GMAs to Disk (file output)";
-            public const string LoadSaveToMemory = GmaMenu + "Test - Load\\Save source folder GMAs in RAM (no file output)";
+            public const string LoadSaveToDisk = Menu + "Test - Load\\Save source folder GMAs to Disk (file output)";
+            public const string LoadSaveToMemory = Menu + "Test - Load\\Save source folder GMAs in RAM (no file output)";
             public const int LoadSaveToDiskPriority = 102;
             public const int LoadSaveToMemoryPriority = 103;
         }
 
         public static class Stage
         {
-            public const string SceneMenu = Const.Menu.Manifold + "Scenes/";
+            public const string Menu = Const.Menu.Manifold + "Scenes/";
             // SceneImportUtility
-            public const string ImportAll = SceneMenu + "Import All Stages";
-            public const string ImportSingle = SceneMenu + "Import Stage (Single)";
-            public const string ImportSingleSelect = SceneMenu + "Import Stage (Single, Select File)";
+            public const string ImportAll = Menu + "Import All Stages";
+            public const string ImportSingle = Menu + "Import Stage (Single)";
+            public const string ImportSingleSelect = Menu + "Import Stage (Single, Select File)";
             public const int ImportAllPriority = 1;
             public const int ImportSinglePriority = 2;
             public const int ImportSingleSelectPriority = 3;
             // SceneExportUtility
-            public const string ExportActiveScene = SceneMenu + "Export (Active Scene)";
+            public const string ExportActiveScene = Menu + "Export (Active Scene)";
             public const int ExportActiveScenePriority = 101;
         }
 
