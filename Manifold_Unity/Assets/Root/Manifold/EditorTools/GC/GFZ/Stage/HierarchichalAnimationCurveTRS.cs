713771e637f7e983279df6c30727a33367fd1b5f
Raphaël Tétreault <raphaeltetreault@outlook.com>
2022-07-10 00:21:55 -0400

    m: Initial implementation of HierarchichalAnimationCurveTRS

--- Manifold_Unity/Assets/Root/Manifold/EditorTools/GC/GFZ/Stage/HierarchichalAnimationCurveTRS.cs
+++ Manifold_Unity/Assets/Root/Manifold/EditorTools/GC/GFZ/Stage/HierarchichalAnimationCurveTRS.cs
@@ -1,12 +1,66 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 namespace Manifold.EditorTools.GC.GFZ.Stage
 {
     [System.Serializable]
     public class HierarchichalAnimationCurveTRS
     {
-        //[field: SerializeField] public AnimationCurveTRS TRS { get; set; } = new();
-        //[field: SerializeField] public HierarchichalAnimationCurveTRS Parent { get; set; } = null;
-        //[field: SerializeField] public HierarchichalAnimationCurveTRS[] Children { get; set; } = null;
+        public Matrix4x4 StaticMatrix { get; set; } = new();
+        public AnimationCurveTRS AnimationTRS { get; set; } = new();
+        public HierarchichalAnimationCurveTRS Parent { get; set; } = null;
+        public HierarchichalAnimationCurveTRS[] Children { get; set; } = null;
+
+        public bool IsRoot => Parent is null;
+        public bool HasParent => Parent is not null;
+        public bool HasChildren => Children.IsNullOrEmpty();
+
+        public Matrix4x4 EvaluateHierarchyMatrix(double time)
+        {
+            // Get parent matrix. This is recursive.
+            var parentMatrix = HasParent
+                ? Parent.EvaluateHierarchyMatrix(time)
+                : Matrix4x4.TRS(Vector3.zero, Quaternion.identity, Vector3.one);
+
+            var selfAnimationMatrix = AnimationTRS.EvaluateMatrix(time);
+            var selfMatrix = StaticMatrix * selfAnimationMatrix;
+
+            var finalMatrix = parentMatrix * selfMatrix;
+            return finalMatrix;
+        }
+
+        public HierarchichalAnimationCurveTRS[] Temp()
+        {
+            // Get the root. From there, make a list of all leaves, recursively
+            // collect all children, then return that.
+
+            var root = GetRoot();
+            var leaves = new List<HierarchichalAnimationCurveTRS>();
+            root.GetLeaves(leaves);
+            return leaves.ToArray();
+        }
+
+        private HierarchichalAnimationCurveTRS GetRoot()
+        {
+            var root = IsRoot ? this : Parent.GetRoot();
+            return root;
+        }
+
+        /// <summary>
+        /// Recursively dig into hierachy and store <paramref name="leaves"/> inside list.
+        /// </summary>
+        /// <param name="leaves"></param>
+        /// <returns></returns>
+        private void GetLeaves(List<HierarchichalAnimationCurveTRS> leaves)
+        {
+            foreach (var child in Children)
+            {
+                if (child.HasChildren)
+                    child.GetLeaves(leaves);
+                else
+                    leaves.Add(child);
+            }
+        }
+
     }
 }
