89667812caf55af665c00c259bbeacbc33df46e6
Raphaël Tétreault <raphaeltetreault@outlook.com>
2022-07-21 20:36:12 -0400

    Get SpiralPathSimple to fairly good working state

--- Manifold_Unity/Assets/Root/Manifold/EditorTools/GC/GFZ/Stage/Track/GfzSpiralPathSimple.cs
+++ Manifold_Unity/Assets/Root/Manifold/EditorTools/GC/GFZ/Stage/Track/GfzSpiralPathSimple.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Unity.Mathematics;
 using UnityEngine;
 
@@ -11,17 +7,7 @@ namespace Manifold.EditorTools.GC.GFZ.Stage.Track
     public class GfzSpiralPathSimple : GfzTrackSegmentRootNode,
         IPositionEvaluable
     {
-        public enum Axes
-        {
-            XZLeft,
-            XZRight,
-            YZUp,
-            YZDown,
-        }
-
-
-
-        [SerializeField] private Axes axes = Axes.XZRight;
+        [SerializeField] private SpiralAxes axes = SpiralAxes.XZRight;
         [SerializeField] private AnimationCurve rollsCurve = new(new(0, 0), new(1, 0));
         [SerializeField] private AnimationCurve scaleX = new(new(0, 60), new(1, 60));
         [SerializeField] private AnimationCurve scaleY = new(new(0, 1), new(1, 1));
@@ -46,31 +32,6 @@ namespace Manifold.EditorTools.GC.GFZ.Stage.Track
             return trs;
         }
 
-        //public override GameCube.GFZ.Stage.TrackSegment CreateTrackSegment()
-        //{
-        //    var trs = animationCurveTRS.CreateDeepCopy();
-
-        //    var trackSegmentRoot = new GameCube.GFZ.Stage.TrackSegment();
-        //    var trackSegmentRZ = new GameCube.GFZ.Stage.TrackSegment();
-        //    trackSegmentRoot.Children = new GameCube.GFZ.Stage.TrackSegment[] { trackSegmentRZ };
-        //    trackSegmentRZ.Children = CreateChildTrackSegments();
-
-        //    trackSegmentRoot.BranchIndex = trackSegmentRZ.BranchIndex = GetBranchIndex();
-
-        //    {
-        //        var trsXY = trs.CreateDeepCopy();
-        //        trsXY.Rotation.z = new AnimationCurve();
-        //        trackSegmentRoot.AnimationCurveTRS = trsXY.ToTrackSegment();
-        //    }
-        //    {
-        //        var trsRZ = new AnimationCurveTRS();
-        //        trsRZ.Rotation.z = trs.Rotation.z;
-        //        trackSegmentRZ.AnimationCurveTRS = trsRZ.ToTrackSegment();
-        //    }
-
-        //    return trackSegmentRoot;
-        //}
-
         public override float GetMaxTime()
         {
             return animationCurveTRS.GetMaxTime();
@@ -81,48 +42,69 @@ namespace Manifold.EditorTools.GC.GFZ.Stage.Track
             return animationCurveTRS.GetMaxTime();
         }
 
-        public Func<float, float, float, Vector3> ComponentsToSpiral(Axes axes)
+        public Func<float, float, float, Vector3> ComponentsToSpiral(SpiralAxes axes)
         {
             switch (axes)
             {
-                case Axes.XZRight: return ComponentsToXZRight;
-                case Axes.XZLeft: return ComponentsToXZLeft;
-                case Axes.YZUp: return ComponentsToXYUp;
-                case Axes.YZDown: return ComponentsToXYDown;
+                case SpiralAxes.XZRight: return ComponentsToXZRight;
+                case SpiralAxes.XZLeft: return ComponentsToXZLeft;
+                case SpiralAxes.YZUp: return ComponentsToXYUp;
+                case SpiralAxes.YZDown: return ComponentsToXYDown;
                 default:
-                    throw new ArgumentException($"Invalid {nameof(Axes)} value.");
+                    throw new ArgumentException($"Invalid {nameof(SpiralAxes)} value.");
             }
         }
-        public Vector3 ComponentsToXZRight(float x, float z, float offset) => new Vector3(-x, offset, z);
-        public Vector3 ComponentsToXZLeft(float x, float z, float offset) => new Vector3(+x, offset, z);
-        public Vector3 ComponentsToXYUp(float y, float z, float offset) => new Vector3(offset, -y, z);
-        public Vector3 ComponentsToXYDown(float y, float z, float offset) => new Vector3(offset, +y, z);
+        public Vector3 ComponentsToXZRight(float cos, float sin, float offset) => new Vector3(-cos, offset, sin);
+        public Vector3 ComponentsToXZLeft(float cos, float sin, float offset) => new Vector3(+cos, offset, sin);
+        public Vector3 ComponentsToXYUp(float cos, float sin, float offset) => new Vector3(offset, -cos, sin);
+        public Vector3 ComponentsToXYDown(float cos, float sin, float offset) => new Vector3(offset, +cos, sin);
 
-        public Func<float, float, float, Vector3> ComponentsToRotation(Axes axes)
+
+        public Func<float, float, (Keyframe[], Keyframe[])> ComponentsToRotationKeys(SpiralAxes axes)
         {
             switch (axes)
             {
-                case Axes.XZRight: return ComponentsToRotationXZRight;
-                case Axes.XZLeft: return ComponentsToRotationXZLeft;
-                case Axes.YZUp:
-                case Axes.YZDown:
+                case SpiralAxes.XZRight: return ComponentsToRotationXZRight;
+                case SpiralAxes.XZLeft: return ComponentsToRotationXZLeft;
+                case SpiralAxes.YZUp: return ComponentsToRotationYZUp;
+                case SpiralAxes.YZDown: return ComponentsToRotationYZDown;
                 default:
-                    throw new ArgumentException($"Invalid {nameof(Axes)} value.");
+                    throw new ArgumentException($"Invalid {nameof(SpiralAxes)} value.");
             }
         }
-        public Vector3 ComponentsToRotationXZRight(float cos, float sin, float slope)
+        public (Keyframe[] rx, Keyframe[] ry) ComponentsToRotationXZRight(float angleSlope, float maxTime)
         {
-            Vector3 normalXZ = new Vector3(sin, 0, cos);
-            Quaternion rotationY = Quaternion.LookRotation(normalXZ, Vector3.up);
-            Quaternion rotationX = Quaternion.AngleAxis(slope, Vector3.right);
-            Quaternion rotation = rotationX * rotationY;
-            Vector3 eulers = rotation.eulerAngles;
-            return eulers;
+            // Subtract slope, add degrees
+            Vector3 rotation = GetRotation().eulerAngles;
+            var rx = new Keyframe[] { new(0, rotation.x), new(0.001f, rotation.x - angleSlope), new(maxTime, rotation.x - angleSlope), };
+            var ry = new Keyframe[] { new(0, rotation.y), new(maxTime, rotation.y + degrees), };
+            return (rx, ry);
         }
-        public Vector3 ComponentsToRotationXZLeft(float cos, float sin, float slope)
-            => ComponentsToRotationXZRight(cos, -sin, slope);
+        public (Keyframe[] rx, Keyframe[] ry) ComponentsToRotationXZLeft(float angleSlope, float maxTime)
+        {
+            // Add slope, subtract degrees
+            Vector3 rotation = GetRotation().eulerAngles;
+            var rx = new Keyframe[] { new(0, rotation.x), new(0.001f, rotation.x + angleSlope), new(maxTime, rotation.x + angleSlope), };
+            var ry = new Keyframe[] { new(0, rotation.y), new(maxTime, rotation.y - degrees), };
+            return (rx, ry);
+        }
 
-
+        public (Keyframe[] rx, Keyframe[] ry) ComponentsToRotationYZUp(float angleSlope, float maxTime)
+        {
+            //
+            Vector3 rotation = GetRotation().eulerAngles;
+            var rx = new Keyframe[] { new(0, rotation.x), new(maxTime, rotation.x - degrees), };
+            var ry = new Keyframe[] { new(0, rotation.y), new(0.001f, rotation.y - angleSlope), new(maxTime, rotation.y - angleSlope), };
+            return (rx, ry);
+        }
+        public (Keyframe[] rx, Keyframe[] ry) ComponentsToRotationYZDown(float angleSlope, float maxTime)
+        {
+            //
+            Vector3 rotation = GetRotation().eulerAngles;
+            var rx = new Keyframe[] { new(0, rotation.x), new(maxTime, rotation.x + degrees), };
+            var ry = new Keyframe[] { new(0, rotation.y), new(0.001f, rotation.y + angleSlope), new(maxTime, rotation.y + angleSlope), };
+            return (rx, ry);
+        }
 
         public AnimationCurve3 CreateScaleCurvesXYZ(float maxTime)
         {
@@ -144,98 +126,78 @@ namespace Manifold.EditorTools.GC.GFZ.Stage.Track
             var startPosition = GetPosition();
             var startRotation = GetRotation().eulerAngles;
             var positionXYZ = new AnimationCurve3();
-            var rotationXYZ = new AnimationCurve3();
 
             var componentToSpiralFunction = ComponentsToSpiral(axes);
-            var componentsToRotation = ComponentsToRotation(axes);
+            var componentsToRotationKeys = ComponentsToRotationKeys(axes);
             var maxTimeRolls = rollsCurve.GetMaxTime();
 
+            Quaternion orientation = GetRotation();
             Vector3 prevEulers = Vector3.zero;
 
             // Length of curve
-            // Todo: archemides spiral calc
-            float length = 2 * Mathf.PI * radius0 * (degrees / 360f);
-            float slope = Mathf.Atan(length / offset1);
+            // Todo: archemides spiral calc?
+            float radiusAverage = ((radius0 + radius1) / 2f);
+            float length = 2 * Mathf.PI * radiusAverage * (degrees / 360f);
+            float angleSlope = Mathf.Atan(offset1 / length) * Mathf.Rad2Deg;
 
             int nKeys = Mathf.CeilToInt(degrees * nKeysPer360Degrees / 360f);
             for (int i = 0; i <= nKeys; i++)
             {
+                // Time / step
                 float time = i / (float)nKeys;
                 float stepDistanceDegrees = time * degrees;
                 float stepDistanceRadians = (stepDistanceDegrees % 360) * Mathf.Deg2Rad; // mod to increase precision
-                int rotationsCompleted = Mathf.FloorToInt(stepDistanceDegrees / 360f);
 
-                // 
+                // Compute radius point
                 float radius = Mathf.Lerp(radius0, radius1, time);
                 float cos = Mathf.Cos(stepDistanceRadians);
                 float sin = Mathf.Sin(stepDistanceRadians);
-
                 // Compute position
                 float componentA0 = cos * radius - radius0;
                 float componentB0 = sin * radius;
                 float offset = Mathf.Lerp(0, offset1, time);
                 Vector3 spiralPosition0 = componentToSpiralFunction(componentA0, componentB0, offset);
-                Vector3 position = startPosition + spiralPosition0;
+                Vector3 position = startPosition + orientation * spiralPosition0;
                 positionXYZ.AddKeys(time, position);
             }
-            positionXYZ.y.SmoothTangents();
 
-            // "Lazy" key time adjustment
-            animationCurveTRS = new AnimationCurveTRS(positionXYZ, rotationXYZ, new());
-            animationCurveTRS.CleanDuplicateKeys();
 
-            float curveLength = length;
-            //float curveLength = (float)CurveUtility.GetDistanceBetweenRepeated(this, 0, 1);
-            var keysPX = positionXYZ.x.GetRenormalizedKeyRangeAndTangents(0, curveLength);
-            var keysPY = positionXYZ.y.GetRenormalizedKeyRangeAndTangents(0, curveLength);
-            var keysPZ = positionXYZ.z.GetRenormalizedKeyRangeAndTangents(0, curveLength);
+            bool isXZ = axes == SpiralAxes.XZLeft || axes == SpiralAxes.XZRight;
+            bool isYZ = axes == SpiralAxes.YZUp || axes == SpiralAxes.YZDown;
 
-            var eRotation = GetRotation().eulerAngles;
-            var keysRX = new Keyframe[]
-            {
-                new(0, eRotation.x + slope),
-                new(curveLength, eRotation.x + slope),
-            };
-            var keysRY = new Keyframe[]
-            {
-                new(0, eRotation.y),
-                new(curveLength, eRotation.y + degrees),
-            };
-            //var keysRX = rotationXYZ.x.GetRenormalizedKeyRangeAndTangents(0, curveLength);
-            //var keysRY = rotationXYZ.y.GetRenormalizedKeyRangeAndTangents(0, curveLength);
-            var keysRZ = rollsCurve.GetRenormalizedKeyRangeAndTangents(0, curveLength);
+            // POSITION
+            var kPosition = GetPosition();
+            var keysPX = isYZ
+                ? new Keyframe[] { new(0, kPosition.x), new(length, kPosition.x + offset1), }
+                : positionXYZ.x.GetRenormalizedKeyRangeAndTangents(0, length);
+            var keysPY = isXZ
+                ? new Keyframe[] { new(0, kPosition.y), new(length, kPosition.y + offset1), }
+                : positionXYZ.y.GetRenormalizedKeyRangeAndTangents(0, length);
+            var keysPZ = positionXYZ.z.GetRenormalizedKeyRangeAndTangents(0, length);
 
+            // ROTATION
+            (var keysRX, var keysRY) = componentsToRotationKeys(angleSlope, length);
+            var keysRZ = rollsCurve.GetRenormalizedKeyRangeAndTangents(0, length);
+
+            // Create TRS
             var trs = new AnimationCurveTRS(
                     new AnimationCurve3(keysPX, keysPY, keysPZ),
                     new AnimationCurve3(keysRX, keysRY, keysRZ),
-                    CreateScaleCurvesXYZ(curveLength));
+                    CreateScaleCurvesXYZ(length));
 
-            // make linear
-            trs.Rotation.y.SmoothTangents();
+            // make linear curves linear
+            if (isXZ)
+            {
+                trs.Position.y.SmoothTangents();
+                trs.Rotation.y.SmoothTangents();
+            }
+            else if (isYZ)
+            {
+                trs.Position.x.SmoothTangents();
+                trs.Rotation.x.SmoothTangents();
+            }
 
             animationCurveTRS = trs;
-        }
-
-        public static float CleanEulerRotation360(float prev, float curr, float nFullRotationsCompleted)
-        {
-            float delta = curr - prev;
-            float offset = 360f * nFullRotationsCompleted;
-
-            // If the difference is lower than max range
-            if (delta > 180)
-                curr -= offset;
-            // if the difference is above the min range
-            else if (delta < -180)
-                curr += offset;
-
-            return curr;
-        }
-        public static float3 CleanEulerRotation3(float3 prev, float3 curr, float nFullRotationsCompleted)
-        {
-            var x = CleanEulerRotation360(prev.x, curr.x, nFullRotationsCompleted);
-            var y = CleanEulerRotation360(prev.y, curr.y, nFullRotationsCompleted);
-            var z = CleanEulerRotation360(prev.z, curr.z, nFullRotationsCompleted);
-            return new float3(x, y, z);
         }
 
     }
---
+++ Manifold_Unity/Assets/Root/Manifold/EditorTools/GC/GFZ/Stage/Track/SpiralAxes.cs
@@ -0,0 +1,10 @@
+﻿namespace Manifold.EditorTools.GC.GFZ.Stage.Track
+{
+    public enum SpiralAxes
+    {
+        XZLeft,
+        XZRight,
+        YZUp,
+        YZDown,
+    }
+}
