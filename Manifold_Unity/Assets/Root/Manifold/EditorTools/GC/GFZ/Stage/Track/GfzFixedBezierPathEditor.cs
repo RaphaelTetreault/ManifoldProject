4b58f324826ca4433b8f2a92ce0030ced23f4bcd
Raphaël Tétreault <raphaeltetreault@hotmail.com>
2022-08-11 17:05:58 -0400

    Simplify conversion from and to world space using tool transform

--- Manifold_Unity/Assets/Root/Manifold/EditorTools/GC/GFZ/Stage/Track/GfzFixedBezierPathEditor.cs
+++ Manifold_Unity/Assets/Root/Manifold/EditorTools/GC/GFZ/Stage/Track/GfzFixedBezierPathEditor.cs
@@ -22,6 +22,7 @@ namespace Manifold.EditorTools.GC.GFZ.Stage.Track
         SerializedProperty selectedIndex;
         SerializedProperty animationCurveTRS;
         SerializedProperty autoGenerateTRS;
+        Transform transform;
 
         private SelectMode selectMode = SelectMode.PositionHandle;
 
@@ -33,11 +34,11 @@ namespace Manifold.EditorTools.GC.GFZ.Stage.Track
         }
 
 
-
         void OnEnable()
         {
             controlPoints = serializedObject.FindProperty(nameof(controlPoints));
             selectedIndex = serializedObject.FindProperty(nameof(selectedIndex));
+            transform = (target as GfzFixedBezierPath).transform;
 
             animationCurveTRS = serializedObject.FindProperty(nameof(animationCurveTRS));
             autoGenerateTRS = serializedObject.FindProperty(nameof(autoGenerateTRS));
@@ -72,25 +73,23 @@ namespace Manifold.EditorTools.GC.GFZ.Stage.Track
             {
                 var controlPoint = editorTarget.GetControlPoint(i);
 
-                var position = controlPoint.position;
-                var orientation = controlPoint.Orientation;
+                var position = WorldPosition(controlPoint);
+                var orientation = WorldOrientation(controlPoint);
                 bool isPressed = Handles.Button(position, orientation, 20f, 30f, Handles.DotHandleCap);
                 if (isPressed)
                 {
                     selectedIndex.intValue = i;
-                    Debug.Log(i);
+                    //Debug.Log(i);
                 }
-
-                if (i == selectedIndex.intValue)
-                {
-                    switch (selectMode)
-                    {
-                        case SelectMode.PositionHandle: PositionHandle(editorTarget, i); break;
-                        case SelectMode.RotationHandle: EulerRotationHandle(editorTarget, i); break;
-                    }
-                }
-                CaptureEditorEvent();
             }
+
+            int index = selectedIndex.intValue;
+            switch (selectMode)
+            {
+                case SelectMode.PositionHandle: PositionHandle(editorTarget, index); break;
+                case SelectMode.RotationHandle: EulerRotationHandle(editorTarget, index); break;
+            }
+            CaptureEditorEvent();
             serializedObject.ApplyModifiedProperties();
         }
 
@@ -124,15 +123,29 @@ namespace Manifold.EditorTools.GC.GFZ.Stage.Track
             }
         }
 
-        /// <summary>
-        /// Maps <paramref name="controlPoint"/> to underlying function.
-        /// </summary>
-        /// <param name="controlPoint"></param>
-        /// <param name="editedPosition"></param>
-        /// <returns></returns>
-        private bool PositionHandle(FixedBezierPoint controlPoint, out Vector3 editedPosition)
-            => HandlesUtility.PositionHandle(controlPoint.position, controlPoint.Orientation, out editedPosition);
-       
+
+        private Vector3 WorldPosition(FixedBezierPoint controlPoint)
+        {
+            var worldPosition = transform.TransformPoint(controlPoint.position);
+            return worldPosition;
+        }
+        private Vector3 LocalPosition(Vector3 position)
+        {
+            var localPosition = transform.InverseTransformPoint(position);
+            return localPosition;
+        }
+
+        private Quaternion WorldOrientation(FixedBezierPoint controlPoint)
+        {
+            var worldOrientation = transform.rotation * controlPoint.Orientation;
+            return worldOrientation;
+        }
+        private Quaternion LocalOrientation(Quaternion orientation)
+        {
+            var localOrientation = Quaternion.Inverse(transform.rotation) * orientation;
+            return localOrientation;
+        }
+
         /// <summary>
         /// Create a position handle for the current <paramref name="tool"/>.
         /// </summary>
@@ -141,22 +154,16 @@ namespace Manifold.EditorTools.GC.GFZ.Stage.Track
         private void PositionHandle(GfzFixedBezierPath tool, int index)
         {
             var controlPoint = tool.GetControlPoint(index);
-            bool didUserMoveHandle = PositionHandle(controlPoint, out Vector3 editedPosition);
+            Vector3 position = WorldPosition(controlPoint);
+            Quaternion orientation = WorldOrientation(controlPoint);
+
+            bool didUserMoveHandle = HandlesUtility.PositionHandle(position, orientation, out Vector3 editedPosition);
             if (didUserMoveHandle)
             {
-                controlPoint.position = editedPosition;
+                controlPoint.position = LocalPosition(editedPosition);
                 tool.SetControlPoint(index, controlPoint);
             }
         }
-
-        /// <summary>
-        /// Maps <paramref name="controlPoint"/> to underlying function.
-        /// </summary>
-        /// <param name="controlPoint"></param>
-        /// <param name="eulerDelta"></param>
-        /// <returns></returns>
-        private bool EulerRotationHandle(FixedBezierPoint controlPoint, out Vector3 eulerDelta)
-            => HandlesUtility.EulerRotationHandle(controlPoint.position, controlPoint.Orientation, out eulerDelta);
 
         /// <summary>
         /// Create a euler rotation handle for the current <paramref name="tool"/>.
@@ -166,7 +173,10 @@ namespace Manifold.EditorTools.GC.GFZ.Stage.Track
         private void EulerRotationHandle(GfzFixedBezierPath tool, int index)
         {
             var controlPoint = tool.GetControlPoint(index);
-            bool didUserMoveHandle = EulerRotationHandle(controlPoint, out Vector3 eulerDelta);
+            Vector3 position = WorldPosition(controlPoint);
+            Quaternion orientation = WorldOrientation(controlPoint);
+
+            bool didUserMoveHandle = HandlesUtility.EulerRotationHandle(position, orientation, out Vector3 eulerDelta);
             if (didUserMoveHandle)
             {
                 controlPoint.EulerOrientation += eulerDelta;
@@ -181,7 +191,7 @@ namespace Manifold.EditorTools.GC.GFZ.Stage.Track
         {
             // https://forum.unity.com/threads/hiding-default-transform-handles.86760/
             int selectedIndex = this.selectedIndex.intValue;
-            bool hideUnityTransformHandle = selectedIndex > 0;
+            bool hideUnityTransformHandle = IsValidIndex(selectedIndex);
             Tools.hidden = hideUnityTransformHandle;
         }
     }
